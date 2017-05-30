8d0bddfa46323f72c305ee5c244b46d62172a665
israelcvidal <israelcvidal@gmail.com>
2017-05-29 22:55:15 -0300

    Adding a better regex to split sentences

--- src/Eiffel_Tower.scala
+++ src/Eiffel_Tower.scala
@@ -8,7 +8,7 @@ import org.apache.log4j.{Level, Logger}
 object Eiffel_Tower {
 
     def main(args: Array[String]) {
-        val k = 20
+        val k = 40
 
         Logger.getLogger("org").setLevel(Level.OFF)
         Logger.getLogger("akka").setLevel(Level.OFF)
@@ -24,20 +24,23 @@ object Eiffel_Tower {
         val file = "Dataset/eiffel-tower-reviews.json"
         val textFile = spark.read.json(file).toDF
 
-        val wordCount = textFile.select("text").rdd
-            .flatMap(line => line.toString().split(" "))
-            .map(word => word.toLowerCase)
-            .filter(word => !stopWords.contains(word))
-            .map(word => (word, 1))
-            .reduceByKey(_ + _)
-
         val toRemove = "[]".toSet
-        val sentenceCount = textFile.select("text").rdd
-//          .flatMap(line => line.toString().split(" and ").)
-          .flatMap(line => line.toString().split("\\.[^.]+"))
-//          .flatMap(line => line.toString().split(" it "))
+
+        val wordCount = textFile.select("text").rdd
+          .flatMap(line => line.toString().split(" "))
           .map(word => word.filterNot(toRemove))
           .map(word => word.toLowerCase)
+          .filter(word => !stopWords.contains(word))
+          .filter(word => word.length > 2)
+          .map(word => (word, 1))
+          .reduceByKey(_ + _)
+
+        val sentenceCount = textFile.select("text").rdd
+          .flatMap(line => line.toString().split("\\.|\\?|\\!|\\,|\\;|\\-"))
+          .map(word => word.filterNot(toRemove))
+          .map(word => word.toLowerCase)
+          .filter(word => word.length > 4)
+          .filter(word => !stopWords.contains(word))
           .map(word => (word, 1))
           .reduceByKey(_ + _)
 //          .reduce(_2 )
@@ -48,8 +51,9 @@ object Eiffel_Tower {
         val topKsentences = sentenceCount
           .sortBy(word => word._2, ascending = false)
           .take(k)
-//        println("Top " + k + " words: ")
-//        topKwords.foreach(println)
+
+        println("Top " + k + " words: ")
+        topKwords.foreach(println)
 
         println("Top " + k + " sentences: ")
         topKsentences.foreach(println)
